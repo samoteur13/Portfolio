b69dd9cab8338669c9e44094decd3ec0d4800865
samoteur13 <s.chabani13@gmail.com>
2022-02-22 14:01:42 +0100

    correction code

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,22 @@
+// const { promiseImpl } = require("ejs");
+
 const textInit = document.getElementById('initText')
 const myText = document.getElementById('myText')
 const animation = document.getElementById('animation')
+const background = document.getElementById('grandEcran')
+console.log(background)
+
+function domReady(f) {
+    if (document.readyState === 'complete') {
+      f();
+    } else {
+      document.addEventListener('DOMContentLoaded', f);
+    }
+  }
+  
+  domReady(function() {
+    background.style.background = "url('./assets/home/picture/pcFace.webp')";
+  });
 
 
 
@@ -14,17 +30,16 @@ setTimeout(() => {// appelle ma fonction avec un delai de 1sec
             await sleep(20);
             myText.innerText = text.substring(0, i);
         }
-        // animation.style.backgroundImage = "url('https://i.pinimg.com/originals/88/15/63/881563d6444b370fa4ceea0c3183bb4c.gif')";
+        //  animation.style.backgroundImage = "url('https://i.pinimg.com/originals/88/15/63/881563d6444b370fa4ceea0c3183bb4c.gif')";
     })()
    
 }, 1000);
 
 
-function sleep(ms) {
+
+
+ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
-for (let i = 0; i < array.length; i++) {
-console.log(formation[i])   
-}
